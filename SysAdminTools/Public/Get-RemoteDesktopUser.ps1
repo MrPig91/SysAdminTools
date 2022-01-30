2c23ec808ff4231c2fd082f4df90a64f7786b57d
MrPig91 <sirius.cleveland@gmail.com>
2022-01-29 21:13:33 -0600

    Fixed Get-RemoteDesktopUser and added help

--- SysAdminTools/Public/Get-RemoteDesktopUser.ps1
+++ SysAdminTools/Public/Get-RemoteDesktopUser.ps1
@@ -1,10 +1,33 @@
-function Get-RemoteDesktopUser{
+<#
+.SYNOPSIS
+    Get the members of the "Remote Desktop Users" group.
+.DESCRIPTION
+    Get the members of the "Remote Desktop Users" group.
+.PARAMETER ComputerName
+    This parameter specifies the name of the computer that want to get the members of the group "Remote Desktop User" for. Default value is the local computer.
+.EXAMPLE
+    PS C:\> Get-RemoteDesktopUser
+
+    Name  Domain    MemberType
+    ----  ------    ----------
+    mrpig WIN-TEST2 UserAccount
+
+    This example grabs all remote desktop users for the local computer.
+.INPUTS
+    string
+        ComputerName
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+.LINK
+    https://github.com/MrPig91/SysAdminTools/wiki/Get%E2%80%90RemoteDesktopUser
+#>
+function Get-RemoteDesktopUser {
     [CmdletBinding()]
     param(
         [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
-        [string[]]$ComputerName,
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [string[]]$UserName
+        [string]$ComputerName = $env:COMPUTERNAME
     )
 
     Begin{
@@ -13,7 +36,7 @@ function Get-RemoteDesktopUser{
 
     Process{
         foreach ($computer in $ComputerName){
-            Get-sysLocalGroupMember -ComputerName $computer -GroupName "Remote Desktop Users"
+            (Get-sysLocalGroup -ComputerName $computer -GroupName "Remote Desktop Users").Member
         } #foreach
     } #process
 }
