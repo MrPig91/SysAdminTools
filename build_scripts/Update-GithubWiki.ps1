38ec38cf46c1065a14161f1715e5a9590c9aaefa
MrPig91 <sirius.cleveland@gmail.com>
2022-01-29 20:42:20 -0600

    changed dash to a hyphen

--- build_scripts/Update-GithubWiki.ps1
+++ build_scripts/Update-GithubWiki.ps1
@@ -7,7 +7,7 @@ $publicFunctions = Get-ChildItem $modulePublicDirecory -File -Filter "*.ps1"
 
 foreach ($command in $publicFunctions){
     Import-Module $command.FullName
-    $commandName = $command.BaseName -Replace "-",'-'
+    $commandName = $command.BaseName -Replace "-",'‐'
     Write-Host "Converting [$CommandName] comment help to markdown"
     $markdown = ConvertTo-MarkdownHelp -Name $command.BaseName -ErrorAction SilentlyContinue
     $markdown | Out-File ".\markdown\$CommandName.md" -Force
